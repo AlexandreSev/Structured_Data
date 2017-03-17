fde7b3d2322adb00a7ad7002d8d55f542e9320dc
AlexandreSev <sevin.alexandre@gmail.com>
2017-03-17 14:10:01 +0100

    first push

---
+++ firstcode.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import tensorflow as tf
+import numpy as np
+from .settings import training_directory
+import xml
+
+print("Hello word")
+
+
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+training_directory = "/home/alex/Documents/strutured data/projet/train"
+
+
+
