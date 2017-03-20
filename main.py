e0103ea15dd8c17772c4b9230efad7f2f42f6c9a
hamid-jalalzai <hamid.jalalzai@gmail.com>
2017-03-20 15:22:25 +0100

    adding learning rate

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from DSOLFUTR.settings import training_directory
 from os.path import join as pjoin
 from DSOLFUTR.model1 import first_model
 import tensorflow as tf
+from time import gmtime, strftime
 
 data = pd.read_csv(pjoin(training_directory, "word.csv"), sep=";", encoding="utf8", 
 	index_col=0)
@@ -39,18 +40,29 @@ for file_name, target in zip(folder1.file, folder1.tag):
 			temp.append(36)
 		list_target.append(temp)
 
-model = first_model()
+model = first_model(learning_rate=1e-4)
 
 prediction = model.predict(list_file)
 print(np.mean(prediction == np.array(list_target)))
 
 one_hot_target = np.array([[np.eye(37)[i] for i in l] for l in list_target])
+
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	for i in range(1):
-		print("Epoch: %r"%i)
+	for i in range(100):
 		model.train_step(list_file, one_hot_target, sess)
+		if i % 10 == 0:
+			print(strftime("%H:%M:%S", gmtime())+" Epoch: %r"%i)
+			prediction = model.predict(list_file)
+			print("score: "+str(np.mean(prediction == np.array(list_target))))
 
-prediction = model.predict(list_file)
-print(np.mean(prediction == np.array(list_target)))
+	save_path = saver.save(sess, "./model.ckpt")
+	print("Model saved in file: %s" % save_path)
+	# https://www.tensorflow.org/programmers_guide/variables#restoring_variables
+
+with tf.Session() as sess:
+	saver.restore(sess, "./model.ckpt")
+	print("Model Loaded.")
 
