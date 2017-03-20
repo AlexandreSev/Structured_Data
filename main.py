3d25e99701c7fb15525f31e303a8a42be0cf83f1
hamid-jalalzai <hamid.jalalzai@gmail.com>
2017-03-20 18:21:37 +0100

    adding rgb2gray directly in the code

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import pandas as pd
 from DSOLFUTR.settings import training_directory
 from os.path import join as pjoin
 from DSOLFUTR.model1 import first_model
+from skimage.color import rgb2gray
 import tensorflow as tf
 from time import gmtime, strftime
+import pickle
 
 data = pd.read_csv(pjoin(training_directory, "word.csv"), sep=";", encoding="utf8", 
 	index_col=0)
@@ -22,7 +24,7 @@ for file_name, target in zip(folder1.file, folder1.tag):
 	temp_name = file_name.split(".")
 	temp_name = "".join(temp_name[:-1] + ["_reshaped.npy"])
 	if firststep:
-		list_file = np.load(pjoin(training_directory, temp_name)).reshape((1, 32, 32, 1))
+		list_file = rgb2gray(np.load(pjoin(training_directory, temp_name))).reshape((1, 32, 32, 1))
 		temp = list(target)
 		temp = [dict_conversion[char.lower()] for char in temp 
 				if char.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]
@@ -32,7 +34,7 @@ for file_name, target in zip(folder1.file, folder1.tag):
 		firststep = False
 	else:
 		list_file = np.vstack((list_file, 
-					np.load(pjoin(training_directory, temp_name)).reshape((1, 32, 32, 1))))
+					rgb2gray(np.load(pjoin(training_directory, temp_name))).reshape((1, 32, 32, 1))))
 		temp = list(target)
 		temp = [dict_conversion[char.lower()] for char in temp 
 				if char.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]
@@ -55,15 +57,20 @@ with tf.Session() as sess:
 		print("Model Loaded.")
 	else:
 		sess.run(tf.global_variables_initializer())
-	for i in range(1, 1000):
+	
+	accuracy = []
+	for i in range(1, 2):
 		model.train_step(list_file, one_hot_target, sess)
 		print(strftime("%H:%M:%S", gmtime())+" Epoch: %r"%i)
-		if i % 1 == 0:
+		if i % 10 == 0:
 			prediction = model.predict(list_file, sess)
 			print("accuracy: "+str(np.mean(prediction == np.array(list_target))))
-
-	save_path = saver.save(sess, "./model.ckpt")
-	print("Model saved in file: %s" % save_path)
+			accuracy.append(np.mean(prediction == np.array(list_target)))
+		if i % 50 == 0:
+			save_path = saver.save(sess, "./model.ckpt")
+			print("Model saved in file: %s" % save_path)
+			with open('accuracy.pickle', 'wb') as file:
+				pickle.dump(accuracy, file, protocol=pickle.HIGHEST_PROTOCOL)
 	# https://www.tensorflow.org/programmers_guide/variables#restoring_variables
 
 
