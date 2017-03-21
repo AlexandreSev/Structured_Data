89d64f55c42a08f7716301dac4fc7fe04f0d1e92
AlexandreSev <sevin.alexandre@gmail.com>
2017-03-21 15:20:08 +0100

    add model 2 and correct the reshaping

---
+++ DSOLFUTR/ngrams.py
@@ -0,0 +1,87 @@
+# coding : utf-8
+from .settings import training_directory
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from nltk.util import ngrams
+from os.path import join as pjoin
+import unicodedata
+
+#training_directory = "/home/alex/Documents/strutured data/projet/train"
+
+
+#  input = shape(4, 4, 256)
+
+def get_ngrams(word=None):
+    if word is None:
+        data = pd.read_csv(pjoin(training_directory, "word.csv"), sep=";", encoding="utf8", index_col=0)
+        list_tag = data['tag']
+    else:
+        list_tag = [word]
+
+
+    tuple_letters = []
+    tuple_bigrams = []
+    tuple_trigrams = []
+    list_letters = []
+    list_bigrams = []
+    list_trigrams = []
+    elements_to_keep = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+
+    for tag in list_tag:
+
+
+        tuple_letters += list(ngrams(tag, 1))
+        tuple_bigrams += list(ngrams(tag, 2))
+        tuple_trigrams += list(ngrams(tag, 3))
+
+    # created list_unique_letter of all the letters and numbers in the text
+
+    for element in tuple_letters :
+        element_used = unicodedata.normalize("NFKD", list(element)[0].lower())
+        if (element_used in elements_to_keep) and (element_used not in list_letters):
+                list_letters.append(element_used)
+
+    list_letters.sort()
+    ##### created list_unique_bigrams of all the bigrams in the text
+
+    for element in tuple_bigrams :
+        list_element=list(element)
+        if (list_element[0].lower() in elements_to_keep) and (list_element[1].lower() in elements_to_keep) :
+            l1 = list_element[0].lower()
+            l2 = list_element[1].lower()
+            element_used = unicodedata.normalize("NFKD", ''.join([l1,l2]))
+            if len(element_used)==2 and (element_used not in list_bigrams):
+                    list_bigrams.append(element_used)
+
+    list_bigrams.sort()
+
+    # print(list_unique_bigrams)
+
+    ##### created list_unique_trigrams of all the trigrams in the text
+
+    for element in tuple_trigrams :
+        list_element=list(element)
+        if (list_element[0].lower() in elements_to_keep) and (list_element[1].lower() in elements_to_keep) and  \
+            (list_element[2].lower() in elements_to_keep) :
+            l1 = list_element[0].lower()
+            l2 = list_element[1].lower()
+            l3 = list_element[2].lower()
+            element_used = unicodedata.normalize("NFKD", ''.join([l1,l2, l3]))
+            if len(element_used)==3 and (element_used not in list_trigrams):
+                list_trigrams.append(element_used)
+
+    list_trigrams.sort()
+
+    return list_letters + list_bigrams + list_trigrams
+
+
+def get_dict_ngrams(list_ngrams):
+    response = {}
+    for i, char in enumerate(list_ngrams):
+        response[char] = i
+    return response
+
+# print(list_unique_trigrams)
---
+++ main2.py
@@ -0,0 +1,70 @@
+#coding: utf- 8
+
+from DSOLFUTR.settings import training_directory
+from os.path import join as pjoin
+from DSOLFUTR.head_model_2 import second_model
+import numpy as np
+import tensorflow as tf
+from skimage.color import rgb2gray
+from DSOLFUTR.ngrams import get_dict_ngrams, get_ngrams
+import pandas as pd
+from time import gmtime, strftime
+import pickle
+
+data = pd.read_csv(pjoin(training_directory, "word.csv"), sep=";", encoding="utf8", 
+	index_col=0)
+
+folder1 = data[data.file.apply(lambda r: (r.split('/')[1] == "1"))]
+
+list_ngrams = get_ngrams()
+
+dict_conversion = get_dict_ngrams(list_ngrams)
+
+
+firststep = True
+
+list_target = np.zeros((len(folder1), len(list_ngrams)))
+
+for nrow, file_name, target in zip(range(len(folder1.file)), folder1.file, folder1.tag):
+	temp_name = file_name.split(".")
+	temp_name = "".join(temp_name[:-1] + ["_reshaped.npy"])
+	if firststep:
+		list_file = np.load(pjoin(training_directory, temp_name)).reshape((1, 32, 32, 1))
+		firststep = False
+	else:
+		list_file = np.vstack((list_file, 
+					np.load(pjoin(training_directory, temp_name)).reshape((1, 32, 32, 1))))
+	list_tag = [ dict_conversion.get(i, None) for i in get_ngrams(target)]
+	for i in list_tag:
+		if i is not None:
+			list_target[nrow, i] = 1
+
+
+with tf.Session() as sess:
+	model = second_model(learning_rate=1e-4)
+	sess.run(tf.global_variables_initializer())
+
+	prediction = model.predict(list_file, sess)
+	print(np.mean(prediction == np.array(list_target)))
+
+	saver = tf.train.Saver()
+
+	if True:
+		saver.restore(sess, "./model2.ckpt")
+		print("Model Loaded.")
+	else:
+		sess.run(tf.global_variables_initializer())
+	
+	accuracy = []
+	for i in range(1, 100000):
+		model.f_train_step(list_file, list_target, sess)
+		print(strftime("%H:%M:%S", gmtime())+" Epoch: %r"%i)
+		if i % 10 == 0:
+			prediction = model.predict(list_file, sess)
+			print("accuracy: "+str(np.mean(prediction == list_target)))
+			accuracy.append(np.mean(prediction == list_target))
+		if i % 50 == 0:
+			save_path = saver.save(sess, "./model2.ckpt")
+			print("Model saved in file: %s" % save_path)
+			with open('accuracy_2.pickle', 'wb') as file:
+				pickle.dump(accuracy, file, protocol=pickle.HIGHEST_PROTOCOL)
