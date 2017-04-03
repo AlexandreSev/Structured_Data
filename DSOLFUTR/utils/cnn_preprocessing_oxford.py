c394301faf98a607cbd482552d082e2394fefbfc
Antoine Isnardy <isnardy.a@gmail.com>
2017-04-03 17:24:45 +0200

    cnn

--- DSOLFUTR/utils/cnn_preprocessing_oxford.py
+++ DSOLFUTR/utils/cnn_preprocessing_oxford.py
@@ -18,13 +18,15 @@ targets_directory = "/mnt/oxford_data/targets/"
 
 model = ResNet50(include_top=False, weights='imagenet', input_shape=(197, 197, 3))
 
-batches = [pjoin(training_directory, path) for path in os.listdir(training_directory) if '.txt' not in path and '.DS_Store' not in path]
-for batch in batches: # Loop through 1..3000 folders
-	
-	subbatches = [pjoin(batch, path) for path in os.listdir(batch) if '.DS_Store' not in path]
-	for subbatch in subbatches:
+batches_dirs = [pjoin(training_directory, path) for path in os.listdir(training_directory) if '.txt' not in path and '.DS_Store' not in path]
+for batch_dir in batches_dirs: # Loop through 1..3000 folders
+	batch_nb = batch_dir.split('/')[-1]
 
-		files = [img for img in os.listdir(subbatch) if 'jpg' in img and '.DS_Store' not in img]
+	subbatches_dirs = [pjoin(batch, path) for path in os.listdir(batch) if '.DS_Store' not in path]
+	for subbatch_dir in subbatches_dirs:
+		subbatch_nb = subbatch_dir.split('/')[-1]
+
+		files = [img for img in os.listdir(subbatch_dir) if 'jpg' in img and '.DS_Store' not in img]
 		files.sort()
 
 		#files_int = [int(img.split(".")[0]) for img in files]
@@ -48,9 +50,9 @@ for batch in batches: # Loop through 1..3000 folders
 		print("batch", batch, "subbatch", subbatch)
 
 	    # Serialize representations
-		h5f = h5py.File(pjoin(representations_directory, "img_emb_" + batch + '_' + subbatch + '.h5'), 'w')
+		h5f = h5py.File(pjoin(representations_directory, "img_emb_" + batch_nb + '_' + subbatch_nb + '.h5'), 'w')
 		h5f.create_dataset('img_emb', data=out_tensor)
 		h5f.close()
 
-		with open(pjoin(targets_directory, "target_" + batch + '_' + subbatch + '.txt'), "wb") as fp:   #Pickling
+		with open(pjoin(targets_directory, "target_" + batch_nb + '_' + subbatch_nb + '.txt'), "wb") as fp:   #Pickling
 			pickle.dump(target, fp)
