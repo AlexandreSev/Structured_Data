fa8d8bdc757279d8cc2e807fc99b8ffab8f037ae
Antoine Isnardy <isnardy.a@gmail.com>
2017-04-05 16:55:29 +0200

    predictions

---
+++ predictions_generation.py
@@ -0,0 +1,13 @@
+from DSOLFUTR.models.model1_resnet_ox import first_head
+from DSOLFUTR.models.model1_resnet_ox import second_head
+
+model1_weights_path='model1.ckpt'
+model2_weights_path='model2.ckpt'
+
+if __name__ == "__main__":
+
+	model1 = first_head()
+	model1.load_weights(model1_weights_path)
+
+	model2 = second_head()
+	model2.load_weights(model2_weights_path)
