fa27007893596f0a97f618c8b139f6b3a8e6cabb
Antoine Isnardy <isnardy.a@gmail.com>
2017-04-05 16:59:27 +0200

    predictions

--- predictions_generation.py
+++ predictions_generation.py
@@ -5,9 +5,11 @@ model1_weights_path='model1.ckpt'
 model2_weights_path='model2.ckpt'
 
 if __name__ == "__main__":
+	with tf.Session() as sess:
+		model1 = first_head()
+		model1.load_weights(model1_weights_path, sess)
 
-	model1 = first_head()
-	model1.load_weights(model1_weights_path)
+		model2 = second_head()
+		model2.load_weights(model2_weights_path, sess)
 
-	model2 = second_head()
-	model2.load_weights(model2_weights_path)
+
